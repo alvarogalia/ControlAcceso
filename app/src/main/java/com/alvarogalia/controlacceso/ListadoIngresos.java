c9f875903f62ee8bc1ea02937f63f986229fef16
alvarogalia <alvaro.galiav@gmail.com>
2019-01-15 20:39:58 -0300

    listado

--- app/src/main/java/com/alvarogalia/controlacceso/ListadoIngresos.java
+++ app/src/main/java/com/alvarogalia/controlacceso/ListadoIngresos.java
@@ -10,9 +10,12 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Space;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -61,8 +64,6 @@ public class ListadoIngresos extends AppCompatActivity {
     private String segundo;
     private String milisegundo;
 
-    TableRow rowCabecera;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,30 +107,40 @@ public class ListadoIngresos extends AppCompatActivity {
         dia = fechahora.split(" ")[0].split("-")[2];
         hora = fechahora.split(" ")[1].split(":")[0];
         minuto = fechahora.split(" ")[1].split(":")[1];
-        segundo = fechahora.split(" ")[1].split(":")[2];
-        milisegundo = fechahora.split(" ")[1].split(":")[3];
-
-        setRows();
 
         txtDesdeAño.setText(año);
         txtDesdeMes.setText(mes);
         txtDesdeDia.setText(dia);
+
+        año = txtDesdeAño.getText().toString().trim();
+        if(año.trim().equals("")){año = null;}
+        mes = txtDesdeMes.getText().toString().trim();
+        if(mes.trim().equals("")){mes = null;}
+        dia = txtDesdeDia.getText().toString().trim();
+        if(dia.trim().equals("")){dia = null;}
+        hora = txtDesdeHora.getText().toString().trim();
+        if(hora.trim().equals("")){hora = null;}
+        minuto = txtDesdeMinuto.getText().toString().trim();
+        if(minuto.trim().equals("")){minuto = null;}
+        setRows();
+
+
 
         Button btnFiltrar = findViewById(R.id.btnFiltrar);
         btnFiltrar.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    año = txtDesdeAño.getText().toString();
-                    if(año.equals("")){año = null;}
-                    mes = txtDesdeMes.getText().toString();
-                    if(mes.equals("")){mes = null;}
-                    dia = txtDesdeDia.getText().toString();
-                    if(dia.equals("")){dia = null;}
-                    hora = txtDesdeHora.getText().toString();
-                    if(hora.equals("")){hora = null;}
-                    minuto = txtDesdeMinuto.getText().toString();
-                    if(minuto.equals("")){minuto = null;}
+                    año = txtDesdeAño.getText().toString().trim();
+                    if(año.trim().equals("")){año = null;}
+                    mes = txtDesdeMes.getText().toString().trim();
+                    if(mes.trim().equals("")){mes = null;}
+                    dia = txtDesdeDia.getText().toString().trim();
+                    if(dia.trim().equals("")){dia = null;}
+                    hora = txtDesdeHora.getText().toString().trim();
+                    if(hora.trim().equals("")){hora = null;}
+                    minuto = txtDesdeMinuto.getText().toString().trim();
+                    if(minuto.trim().equals("")){minuto = null;}
                     setRows();
                 }
                 return true;
@@ -142,7 +153,7 @@ public class ListadoIngresos extends AppCompatActivity {
         tblIngresos.removeAllViews();
 
 
-        rowCabecera = new TableRow(ListadoIngresos.this);
+        TableRow rowCabecera = new TableRow(ListadoIngresos.this);
 
         rowCabecera.setBackgroundColor(Color.parseColor(colorTblBackgroundHead));
 
@@ -154,7 +165,7 @@ public class ListadoIngresos extends AppCompatActivity {
         TextView tv5 = new TextView(ListadoIngresos.this);
         TextView tv6 = new TextView(ListadoIngresos.this);
 
-        tv0.setText("FECHA HORA");
+        tv0.setText("FECHA\nHORA");
         tv1.setText("RUT");
         tv2.setText("NOMBRE");
         tv3.setText("DESTINO");
@@ -171,14 +182,28 @@ public class ListadoIngresos extends AppCompatActivity {
         tv6.setTextColor(Color.parseColor(colorTblTextHead));
 
         rowCabecera.addView(tv0);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv1);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv2);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv3);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv4);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv5);
+        rowCabecera.addView(new Space(this));
         rowCabecera.addView(tv6);
 
         tblIngresos.addView(rowCabecera);
+
+        setLayoutStyle(1, tv0);
+        setLayoutStyle(1, tv1);
+        setLayoutStyle(1, tv2);
+        setLayoutStyle(1, tv3);
+        setLayoutStyle(1, tv4);
+        setLayoutStyle(1, tv5);
+        setLayoutStyle(1, tv6);
 
         DatabaseReference qrefHistNew = database.getReference("HOLDING").child(holding).child("VISITAS");
         if(año != null){
@@ -191,9 +216,6 @@ public class ListadoIngresos extends AppCompatActivity {
                         qrefHistNew = qrefHistNew.child(hora);
                         if(minuto != null){
                             qrefHistNew = qrefHistNew.child(minuto);
-                            if(segundo != null){
-                                qrefHistNew = qrefHistNew.child(segundo);
-                            }
                         }
                     }
                 }
@@ -206,65 +228,125 @@ public class ListadoIngresos extends AppCompatActivity {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getRef().getPath().toString());
 
-                for (DataSnapshot horas: dataSnapshot.getChildren()) {
-                    final String hora = horas.getKey();
-                    for(DataSnapshot minutos : horas.getChildren()){
-                        final String minuto = minutos.getKey();
-                        for(DataSnapshot segundos : minutos.getChildren()){
-                            final String segundo = segundos.getKey();
+                if(minuto != null){
+                    minuto = dataSnapshot.getKey();
+                        for(DataSnapshot segundos : dataSnapshot.getChildren()){
+                            segundo = segundos.getKey();
                             for(DataSnapshot millis : segundos.getChildren()){
                                 Visita visita = millis.getValue(Visita.class);
-                                TableRow row = new TableRow(ListadoIngresos.this);
-
-                                row.setBackgroundColor(Color.parseColor(colorTblBackground));
-
-                                TextView tv0 = new TextView(ListadoIngresos.this);
-                                tv0.setText(hora + ":" + minuto+ ":" + segundo);
-                                TextView tv1 = new TextView(ListadoIngresos.this);
-                                TextView tv2 = new TextView(ListadoIngresos.this);
-                                TextView tv3 = new TextView(ListadoIngresos.this);
-
-                                if(visita.PERSONA != null){
-                                    tv1.setText(visita.PERSONA.rut+"-"+visita.PERSONA.DV);
-                                    tv2.setText(visita.PERSONA.nombre);
-                                    tv3.setText(visita.PERSONA.destino);
-                                    tv2.setTextColor(Color.parseColor(colorTblText));
-                                    tv3.setTextColor(Color.parseColor(colorTblText));
-                                }
-
-                                TextView tv4 = new TextView(ListadoIngresos.this);
-                                TextView tv5 = new TextView(ListadoIngresos.this);
-                                TextView tv6 = new TextView(ListadoIngresos.this);
-
-                                if(visita.VEHICULO != null){
-
-                                    tv4.setText(visita.VEHICULO.ppu);
-                                    tv5.setText(visita.VEHICULO.tipoVehiculo);
-                                    tv6.setText(visita.VEHICULO.color);
-
-                                    tv4.setTextColor(Color.parseColor(colorTblText));
-                                    tv5.setTextColor(Color.parseColor(colorTblText));
-                                    tv6.setTextColor(Color.parseColor(colorTblText));
-                                }
-                                tv0.setTextColor(Color.parseColor(colorTblText));
-                                tv1.setTextColor(Color.parseColor(colorTblText));
-
-                                row.addView(tv0);
-                                row.addView(tv1);
-                                row.addView(tv2);
-                                row.addView(tv3);
-                                row.addView(tv4);
-                                row.addView(tv5);
-                                row.addView(tv6);
-
-                                row.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        System.out.println(año+mes+dia+hora+minuto+segundo);
+                                addVisita(visita);
+                            }
+                        }
+                }else{
+                    if(hora != null){
+                        hora = dataSnapshot.getKey();
+                            for(DataSnapshot minutos : dataSnapshot.getChildren()){
+                                minuto = minutos.getKey();
+                                for(DataSnapshot segundos : minutos.getChildren()){
+                                    segundo = segundos.getKey();
+                                    for(DataSnapshot millis : segundos.getChildren()){
+                                        Visita visita = millis.getValue(Visita.class);
+                                        addVisita(visita);
                                     }
-                                });
-                                tblIngresos.addView(row);
+                                }
+                            }
+                    }else{
+                        if(dia != null){
+                            dia = dataSnapshot.getKey();
+                                for(DataSnapshot horas : dataSnapshot.getChildren()){
+                                    hora = horas.getKey();
+                                    for(DataSnapshot minutos : horas.getChildren()){
+                                        minuto = minutos.getKey();
+                                        for(DataSnapshot segundos : minutos.getChildren()){
+                                            segundo = segundos.getKey();
+                                            for(DataSnapshot millis : segundos.getChildren()){
+                                                Visita visita = millis.getValue(Visita.class);
+                                                addVisita(visita);
+                                            }
+                                        }
+                                    }
+                                }
+                        }else{
+                            if(mes != null){
+                                mes = dataSnapshot.getKey();
+                                    for(DataSnapshot dias : dataSnapshot.getChildren()){
+                                        dia = dias.getKey();
+                                        for(DataSnapshot horas : dias.getChildren()){
+                                            hora = horas.getKey();
+                                            for(DataSnapshot minutos : horas.getChildren()){
+                                                minuto = minutos.getKey();
+                                                for(DataSnapshot segundos : minutos.getChildren()){
+                                                    segundo = segundos.getKey();
+                                                    for(DataSnapshot millis : segundos.getChildren()){
+                                                        Visita visita = millis.getValue(Visita.class);
+                                                        addVisita(visita);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                }
+                            }else{
+                                if(año != null){
+                                    año = dataSnapshot.getKey();
+                                        for(DataSnapshot meses : dataSnapshot.getChildren()){
+                                            mes = meses.getKey();
+                                            for(DataSnapshot dias : meses.getChildren()){
+                                                dia = dias.getKey();
+                                                for(DataSnapshot horas : dias.getChildren()){
+                                                    hora = horas.getKey();
+                                                    for(DataSnapshot minutos : horas.getChildren()){
+                                                        minuto = minutos.getKey();
+                                                        for(DataSnapshot segundos : minutos.getChildren()){
+                                                            segundo = segundos.getKey();
+                                                            for(DataSnapshot millis : segundos.getChildren()){
+                                                                Visita visita = millis.getValue(Visita.class);
+                                                                addVisita(visita);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                }else{
+                                    for(DataSnapshot años : dataSnapshot.getChildren()){
+                                        año = años.getKey();
+                                        for(DataSnapshot meses : años.getChildren()){
+                                            mes = meses.getKey();
+                                            for(DataSnapshot dias : meses.getChildren()){
+                                                dia = dias.getKey();
+                                                for(DataSnapshot horas : dias.getChildren()){
+                                                    hora = horas.getKey();
+                                                    for(DataSnapshot minutos : horas.getChildren()){
+                                                        minuto = minutos.getKey();
+                                                        for(DataSnapshot segundos : minutos.getChildren()){
+                                                            segundo = segundos.getKey();
+                                                            for(DataSnapshot millis : segundos.getChildren()){
+                                                                Visita visita = millis.getValue(Visita.class);
+                                                                addVisita(visita);
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                System.out.println("CANTIDAD ROWS:" + tblIngresos.getChildCount());
+                for(int i = 0; i < tblIngresos.getChildCount();i++){
+                    if(tblIngresos.getChildAt(i).getClass().getName().endsWith("TableRow")){
+                        TableRow tableRow = (TableRow) tblIngresos.getChildAt(i);
+                        System.out.println("CANTIDAD CELLS:" + tableRow.getChildCount());
+                        for(int j = 0; j < tableRow.getChildCount();j++){
+                            if(tableRow.getChildAt(j).getClass().getName().endsWith("TextView")){
+                                TextView textView = (TextView) tableRow.getChildAt(j);
+                                setLayoutStyle(1, textView);
                             }
                         }
                     }
@@ -277,6 +359,73 @@ public class ListadoIngresos extends AppCompatActivity {
             }
         });
     }
+    public void addVisita(Visita visita){
+
+        TableRow row = new TableRow(ListadoIngresos.this);
+        TextView tv0 = new TextView(ListadoIngresos.this);
+
+        tv0.setText(año+"-"+mes+"-"+dia+"\n"+hora+":"+minuto+":"+segundo);
+        TextView tv1 = new TextView(ListadoIngresos.this);
+        TextView tv2 = new TextView(ListadoIngresos.this);
+        TextView tv3 = new TextView(ListadoIngresos.this);
+
+        if(visita.PERSONA != null){
+            tv1.setText(visita.PERSONA.rut+"-"+visita.PERSONA.DV);
+            tv2.setText(visita.PERSONA.nombre);
+            tv3.setText(visita.PERSONA.destino);
+        }
+
+        TextView tv4 = new TextView(ListadoIngresos.this);
+        TextView tv5 = new TextView(ListadoIngresos.this);
+        TextView tv6 = new TextView(ListadoIngresos.this);
+
+        if(visita.VEHICULO != null){
+
+            tv4.setText(visita.VEHICULO.ppu);
+            tv5.setText(visita.VEHICULO.tipoVehiculo);
+            tv6.setText(visita.VEHICULO.color);
+        }
+
+        row.addView(tv0);
+        row.addView(new Space(this));
+        row.addView(tv1);
+        row.addView(new Space(this));
+        row.addView(tv2);
+        row.addView(new Space(this));
+        row.addView(tv3);
+        row.addView(new Space(this));
+        row.addView(tv4);
+        row.addView(new Space(this));
+        row.addView(tv5);
+        row.addView(new Space(this));
+        row.addView(tv6);
+
+
+        row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(año+"-"+mes+"-"+dia+" "+hora+":"+minuto+":"+segundo);
+            }
+        });
+        tblIngresos.addView(row);
+
+
+    }
+
+    private void setLayoutStyle(int i, TextView tv) {
+        int rowHeight = Integer.parseInt(preferences.getString("TABLE_ROW_HEIGHT", "80"));
+        int rowTextSize = Integer.parseInt(preferences.getString("TABLE_ROW_FONT_SIZE", "15"));
+
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) tv.getLayoutParams();
+        params.setMargins(i,i,i,i);
+        tv.setLayoutParams(params);
+
+        tv.setTextColor(Color.parseColor(colorTblText));
+        tv.setBackgroundColor(Color.parseColor(colorTblBackground));
+        tv.setHeight(rowHeight);
+        tv.setTextSize(rowTextSize);
+    }
+
     public void setChildrens(LinearLayout ll){
         for(int i = 0; i < ll.getChildCount(); i++){
             View view = ll.getChildAt(i);
